41ca72f926acce13bba014091ead48a6c8423c19
kaykf <kaykfelipe071@gmail.com>
2023-12-23 14:05:41 -0300

    inicio js

---
+++ javascript-mod1/aula10/index.js
@@ -0,0 +1,10 @@
+let nome= "kayk"
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+window.alert(nome)
+let meuNome =window.prompt("insira o seu nome")
+console.log(meuNome)
+let confime = window.confirm("Confirme essa ação")
+.alert("cuidado")
---
+++ javascript-mod1/aula9/index.js
@@ -0,0 +1,5 @@
+let nome = "kayk"
+let xx = 5
+
+console.log(nome)
+console.log(xx)
