b57d1ccfa9260b0cd03b5004cd5f07cff78b9138
kaykf <kaykfelipe071@gmail.com>
2023-12-23 22:22:54 -0300

    estruturas de controle

---
+++ javascript-mod1/aula16/index.js
@@ -0,0 +1,20 @@
+const idade = prompt("Informe sua idade:")
+
+
+if( idade>18){
+    alert("Você é maior de idade!")
+    if(idade>50){
+        alert("Tué véio pra caralho!!!")
+    }
+}else if(idade==17){
+    alert("Tente ano que vem kkkkkk")
+}else if(idade<=8){
+    alert("Você ainda é pirraio(a)")
+}else if(idade<18){
+    alert("Você é menos de idade!!")
+}else{
+    alert("..")
+}
+
+const resultado = (6===6) ? "Verdadeiro": "Falso"
+console.log(resultado)
