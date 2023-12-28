5361a533f8033893da600f4f97b810f58272174b
kaykf <kaykfelipe071@gmail.com>
2023-12-28 19:45:38 -0300

    basico arrays

---
+++ javascript-mod2/aula32/array.js
@@ -0,0 +1,10 @@
+const listaDeCompras = []
+console.log(listaDeCompras)
+
+listaDeCompras[0] ="arroz"
+listaDeCompras[1] = "Feijão"
+listaDeCompras[2] = 7
+listaDeCompras[5] = "Batata"
+console.log(listaDeCompras)
+
+console.log(listaDeCompras[3])
