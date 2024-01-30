a1ef15f53d2801c22f2ad7750990f145601d9433
kaykf <kaykfelipe071@gmail.com>
2024-01-30 17:29:34 -0300

    tudo pronto
    feito por minhas mãos

--- javascript-mod3/16-estilos com dom/index.js
+++ javascript-mod3/16-estilos com dom/index.js
@@ -8,5 +8,11 @@ function useDarkTheme(){
   document.body.style.backgroundColor= '#212529'
 }
 
+function switchTheme(){
+  document.body.classList.toggle('is-light')
+  document.body.classList.toggle('is-dark')
+}
+
 document.getElementById("lightBtn").addEventListener('click', useLightTheme)
 document.getElementById("darkBtn").addEventListener('click', useDarkTheme )
+document.getElementById("switchBtn").addEventListener('click', switchTheme)
