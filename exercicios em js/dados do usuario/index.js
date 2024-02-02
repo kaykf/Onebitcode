3a36e7d13e39dd036b87385692610f071b227242
kaykf <kaykfelipe071@gmail.com>
2024-02-02 17:06:27 -0300

    primeiro ex completo

---
+++ exercicios em js/dados do usuario/index.js
@@ -0,0 +1,14 @@
+alert('Bem-vindo(a)! A seguir pediremos que informe alguns dados.')
+
+const nome = prompt("Informe seu nome:")
+
+const idade = prompt("Informe a sua idade:")
+
+const conIdade = confirm("Você realmente tem "+ idade+" anos?")
+
+if(confirm = true){
+    repIdade=idade
+    alert("Nome: "+ nome +"\nIdade: " + idade+ "\nConfimação da idade: "+ repIdade)
+}else{
+    alert("Porfavor coloque a sua verdadeira idade")
+}
