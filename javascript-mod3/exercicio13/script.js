2dcfd4ccbbfbe6538ccc03487c516cda0b47addc
kaykf <kaykfelipe071@gmail.com>
2024-02-16 19:52:53 -0300

    quase pronto
    ja condigo alterar a vez do jogador, e mostrar que um dos dois venceu a partida ou empatou

--- javascript-mod3/exercicio13/script.js
+++ javascript-mod3/exercicio13/script.js
@@ -19,6 +19,27 @@ function initializeGame(){
     })
 }
 
+function getWinRegions(){
+    const winRegions = []
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2])
+    winRegions.push("0.0", "0.1", "0.2")
+  if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2])
+    winRegions.push("1.0", "1.1", "1.2")
+  if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2])
+    winRegions.push("2.0", "2.1", "2.2")
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0])
+    winRegions.push("0.0", "1.0", "2.0")
+  if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1])
+    winRegions.push("0.1", "1.1", "2.1")
+  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2])
+    winRegions.push("0.2", "1.2", "2.2")
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2])
+    winRegions.push("0.0", "1.1", "2.2")
+  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0])
+    winRegions.push("0.2", "1.1", "2.0")
+  return winRegions
+}
+
 function disableRegion(element){
     element.style.cursor = 'default'
     element.removeEventListener('click', handBoardClick)
@@ -40,6 +61,15 @@ function handBoardClick(ev){
     console.clear()
     console.table(vBoard)
     disableRegion(span)
+    const winRegions = getWinRegions()
+    if(winRegions.length > 0){
+        console.log("Venceu!!")
+    }else if(vBoard.flat().includes('')){
+        turnPlayer = turnPlayer ==='player1'? 'player2' : 'player1'
+        updateTitle()
+    }else{
+        document.querySelector('h2').innerHTML = 'Empate!'
+    }
 }
 
 document.getElementById('start').addEventListener('click', initializeGame)
