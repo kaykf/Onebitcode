2f25ce5ddfbe83fa559f92b23877a978f78ec66d
kaykf <kaykfelipe071@gmail.com>
2024-02-27 20:53:04 -0300

    desestruturação de objetos e arrays

---
+++ JAVASCRIPT/javascript-mod4/aula-06/script.js
@@ -0,0 +1,28 @@
+const person = {
+  name: "Luke",
+  job: "Farmer",
+  parents: ["Anakin", "Padme"]
+}
+
+const name = person.name
+
+const {job, parents} = person
+
+console.log(name,job,parents)
+
+const [pai , mae] = parents
+
+console.log(pai,mae)
+
+function createUser({name, job, parents}){
+  const id = Math.floor(Math.random()*9999)
+  return{
+    id,
+    name,
+    job,
+    parents
+  }
+}
+const luke = createUser(person)
+
+console.log(luke)
