a773182794d9d9768025aa045ec34a37681fab1b
kaykf <kaykfelipe071@gmail.com>
2024-03-07 19:23:07 -0300

    ESM non inline

--- JAVASCRIPT/javascript-mod4/aula-18/index.js
+++ JAVASCRIPT/javascript-mod4/aula-18/index.js
@@ -1,9 +1,13 @@
 import { inline } from "./inline.js"
 import padrao from "./inline.js"
+import { group, a, b, c } from "./non-inline.js"
+import exportDefault from "./non-inline.js"
 
 padrao()
 inline()
-defaultInline()
 
 group()
+a()
+b()
+c()
 exportDefault()
--- JAVASCRIPT/javascript-mod4/aula-18/non-inline.js
+++ JAVASCRIPT/javascript-mod4/aula-18/non-inline.js
@@ -1,9 +1,14 @@
 function group (){
     console.log('Export nomeado não-inline (agrupado)')
 }
+function a(){console.log('A')}
+function b(){console.log('B')}
+function c(){console.log('C')}
 
 function exportDefault(){
     console.log('Export default não-inline')
 }
 
-export {group}
+export {group, a, b, c}
+
+export default exportDefault
