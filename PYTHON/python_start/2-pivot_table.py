f265e76639576cb1ba8dcc5d194787f3bf256044
kaykf <kaykfelipe071@gmail.com>
2024-05-19 23:40:46 -0300

    manipulando os primeiros dados com python

--- PYTHON/python_start/2-pivot_table.py
+++ PYTHON/python_start/2-pivot_table.py
@@ -1,9 +1,19 @@
 import pandas as pd
 
-data = pd.read_excel("data/VendaCarros.xlsx")
+data = pd.read_excel('python_start/data2/VendaCarros.xlsx')
 print(type(data))
+
 
 df = data[["Fabricante", "ValorVenda" ,"Ano"]]
 print(df)
 
+pivot_table = df.pivot_table(
+    index="Ano",
+    columns="Fabricante",
+    values="ValorVenda",
+    aggfunc='sum' 
+)
 
+print(pivot_table)
+
+pivot_table.to_excel('python_start/data2/pivot_table.xlsx', "Relátorio")
